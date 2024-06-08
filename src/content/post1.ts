a0ae9a6c46a47292073b33268e89c6df5d4c7040
hojay <jeahonglee@gmail.com>
2024-06-08 21:55:43 +0900

    content repository and dummy posts setup

---
+++ src/content/post1.ts
@@ -0,0 +1,14 @@
+import { Post } from '../types/post';
+
+const post: Post = {
+  slug: 'post1',
+  date: '2024-06-01T09:01:00Z',
+  title: 'My first post',
+  description: 'This is my first post',
+  thumbnail: '',
+  content: `
+  
+  `,
+};
+
+export default post;
---
+++ src/content/post2.ts
@@ -0,0 +1,14 @@
+import { Post } from '../types/post';
+
+const post: Post = {
+  slug: 'post2',
+  date: '2024-06-01T09:01:00Z',
+  title: 'This is my second post',
+  description: 'Description for post 2 is : This is my second post',
+  thumbnail: '',
+  content: `
+  
+  `,
+};
+
+export default post;
