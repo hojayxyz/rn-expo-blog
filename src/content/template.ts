63eda75ed9a70a80b7319f02a5f48a7f93be0bec
hojay <jeahonglee@gmail.com>
2024-06-08 21:48:28 +0900

    Post template

---
+++ src/content/template.ts
@@ -0,0 +1,14 @@
+import { Post } from '../types/post';
+
+const post: Post = {
+  slug: '',
+  date: '2024-06-01T09:00:00Z',
+  title: '',
+  description: '',
+  thumbnail: '',
+  content: `
+  
+  `,
+};
+
+export default post;
